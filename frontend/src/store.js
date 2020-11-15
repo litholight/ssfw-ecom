13b3b3f033437876903c2e9b4eabe4fd191f809f
Sean Stoneburner <seanstoneburner@Seans-MacBook-Pro.local>
2020-11-15 13:34:52 -0500

    registration and screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,13 +4,14 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer } from './reducers/productReducers';
 import { productDetailsReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
-import { userLoginReducer } from './reducers/userReducers';
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
